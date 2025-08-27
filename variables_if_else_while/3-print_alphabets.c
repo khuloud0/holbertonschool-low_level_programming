48c9a7df1db7453d2757b8ed1187e0104e4b5bb6
root <root@ip-10-42-44-228.ec2.internal>
2025-08-27 13:40:09 +0000

    Add 3-print_alphabets.c: print lowercase then uppercase alphabet

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print the alphabet in lowercase,
+ * then in uppercase, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
