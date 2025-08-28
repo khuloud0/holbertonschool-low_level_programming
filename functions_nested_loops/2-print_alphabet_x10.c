a753e6a54905031dcbf5d862217d20ecd123d9a2
root <root@ip-10-42-235-188.ec2.internal>
2025-08-28 05:58:13 +0000

    Fix Betty style and add function print_alphabet_x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,21 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints 10 times the alphabet in lowercase
+ * print_alphabet_x10 - prints the alphabet 10 times in lowercase
+ *
+ * Return: void
  */
 void print_alphabet_x10(void)
 {
-    int i, ch;
+	int i;
+	char c;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (ch = 'a'; ch <= 'z'; ch++)
-        {
-            _putchar(ch);
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
 }
