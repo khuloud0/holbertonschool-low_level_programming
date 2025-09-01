67bd619eda2977f5e2a1dc94dae7ec1d35e59674
root <root@ip-10-42-193-180.ec2.internal>
2025-09-01 12:51:42 +0000

    Add task 3: _puts using _putchar

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -8,6 +8,11 @@ void swap_int(int *a, int *b);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
 
 
 #endif /* MAIN_H */
