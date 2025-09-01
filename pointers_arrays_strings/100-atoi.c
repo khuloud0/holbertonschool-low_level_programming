d24904ecf77fce552f799a646729ad70efcbe22d
root <root@ip-10-42-193-180.ec2.internal>
2025-09-01 15:37:55 +0000

    Fix atoi to handle INT_MIN correctly

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,22 +8,23 @@
  */
 int _atoi(char *s)
 {
-	int i = 0;
-	int sign = 1;
-	int result = 0;
+    int i = 0, sign = 1;
+    unsigned int result = 0;
 
-	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
-	{
-		if (s[i] == '-')
-			sign *= -1;
-		i++;
-	}
+    while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+    {
+        if (s[i] == '-')
+            sign *= -1;
+        i++;
+    }
 
-	while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
-	{
-		result = result * 10 + (s[i] - '0');
-		i++;
-	}
+    while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
+    {
+        result = result * 10 + (s[i] - '0');
+        i++;
+    }
 
-	return (result * sign);
+    if (sign < 0)
+        return (-result);
+    return (result);
 }
