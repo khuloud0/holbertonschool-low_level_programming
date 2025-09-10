6313b1748de9485bfbf0a314bee31c041dedf826
root <root@ip-10-42-93-38.ec2.internal>
2025-09-10 12:22:11 +0000

    Add _print_rev_recursion.c and 1-main.c with working test

---
+++ recursion/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - test the _print_rev_recursion function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_print_rev_recursion("Holberton");
+	_putchar('\n');
+	return (0);
+}
