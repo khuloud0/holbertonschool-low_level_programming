8154ef0d618f920c6184caf9794f8602efc3cb6b
root <root@ip-10-42-51-222.ec2.internal>
2025-09-15 14:48:05 +0000

    Fix: update 1-string_nconcat.c with Betty style compliance

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,44 +1,44 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * string_nconcat - concatenates two strings
  * @s1: first string
  * @s2: second string
- * @n: maximum number of bytes of s2 to concatenate
+ * @n: number of bytes from s2 to concatenate
  *
- * Return: pointer to newly allocated memory with s1 followed by s2,
- *         or NULL if it fails
+ * Return: pointer to newly allocated space containing s1 + s2,
+ * or NULL if it fails
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int i, j, len1 = 0, len2 = 0;
-    char *new_str;
+	unsigned int i, j, len1 = 0, len2 = 0;
+	char *s;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    while (s1[len1] != '\0')
-        len1++;
-    while (s2[len2] != '\0')
-        len2++;
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
 
-    if (n >= len2)
-        n = len2;
+	if (n >= len2)
+		n = len2;
 
-    new_str = malloc(sizeof(char) * (len1 + n + 1));
-    if (new_str == NULL)
-        return (NULL);
+	s = malloc(sizeof(char) * (len1 + n + 1));
+	if (s == NULL)
+		return (NULL);
 
-    for (i = 0; i < len1; i++)
-        new_str[i] = s1[i];
+	for (i = 0; i < len1; i++)
+		s[i] = s1[i];
 
-    for (j = 0; j < n; j++)
-        new_str[i + j] = s2[j];
+	for (j = 0; j < n; j++)
+		s[i + j] = s2[j];
 
-    new_str[i + j] = '\0';
+	s[i + j] = '\0';
 
-    return (new_str);
+	return (s);
 }
