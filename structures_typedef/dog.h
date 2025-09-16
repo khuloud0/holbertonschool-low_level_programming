85429d1135b36c35053348015c44b25f6ac9a02c
root <root@ip-10-42-170-220.ec2.internal>
2025-09-16 13:28:09 +0000

    Update dog.h and 1-init_dog.c for Betty compliance

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,13 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog
+/**
+ * struct dog - struct that stores dog information
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+typedef struct dog
 {
-    char *name;
-    float age;
-    char *owner;
-};
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
+/* prototype */
+void init_dog(dog_t *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */
