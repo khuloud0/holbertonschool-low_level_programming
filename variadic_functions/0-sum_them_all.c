ed0e4114ad9741bb206b180f3131e9cd78763b46
root <root@ip-10-42-119-67.ec2.internal>
2025-09-24 12:39:57 +0000

    Add sum_them_all task

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -9,19 +9,17 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-    unsigned int i;
-    int sum = 0;
-    va_list args;
+	va_list args;
+	unsigned int i;
+	int sum = 0;
 
-    if (n == 0)
-        return (0);
+	if (n == 0)
+		return (0);
 
-    va_start(args, n);
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
+	va_end(args);
 
-    for (i = 0; i < n; i++)
-        sum += va_arg(args, int);
-
-    va_end(args);
-
-    return (sum);
+	return (sum);
 }
