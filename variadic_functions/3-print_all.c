1fa8d711082f328678a5588aa1687e11ea14fb93
root <root@ip-10-42-197-206.ec2.internal>
2025-09-25 14:02:18 +0000

    Refactor functions to comply with Betty style

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,11 +2,16 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments
+ */
 void print_all(const char * const format, ...)
 {
 	va_list args;
 	unsigned int i = 0;
-	char *str, *sep = "";
+	char *str;
+	char *sep = "";
 
 	if (!format)
 	{
@@ -20,19 +25,31 @@ void print_all(const char * const format, ...)
 	{
 		switch (format[i])
 		{
-			case 'c': printf("%s%c", sep, va_arg(args, int)); break;
-			case 'i': printf("%s%d", sep, va_arg(args, int)); break;
-			case 'f': printf("%s%f", sep, va_arg(args, double)); break;
+			case 'c':
+				printf("%s%c", sep, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", sep, va_arg(args, double));
+				break;
 			case 's':
 				str = va_arg(args, char *);
-				if (!str) str = "(nil)";
+				if (!str)
+				{
+					str = "(nil)";
+				}
 				printf("%s%s", sep, str);
 				break;
-			default: i++; continue;
+			default:
+				i++;
+				continue;
 		}
 		sep = ", ";
 		i++;
 	}
+
 	va_end(args);
 	printf("\n");
 }
