d9b2fc97ff72d383e8dd63d1816a0910e0243096
root <root@ip-10-42-71-158.ec2.internal>
2025-10-05 12:55:52 +0000

    Add solution for 1-list_len task

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,9 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
@@ -15,11 +15,12 @@
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif
