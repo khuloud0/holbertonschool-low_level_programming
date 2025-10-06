e7ad6949f320b03e2029856ed173def8ec9fbfba
root <root@ip-10-42-194-6.ec2.internal>
2025-10-06 13:50:59 +0000

    Add task 4 free_list function with Betty fixed

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -5,11 +5,9 @@
 
 /**
  * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
+ * @str: string (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
- *
- * Description: singly linked list node structure
  */
 typedef struct list_s
 {
